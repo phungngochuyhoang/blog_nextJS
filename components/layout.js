e72e3d2a128487bf794270a188e2651047ddcd4c
hoang phung <hoang.phung@cloudconnecttech.com>
2021-04-15 22:12:41 +0700

    code

---
+++ components/layout.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Layout.module.css'
+
+const Layout = function ({children}) {
+
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>{children}</main>
+        </div>
+    );
+}
+
+export default Layout;
